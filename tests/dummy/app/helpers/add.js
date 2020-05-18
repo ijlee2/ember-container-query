9c022cac8aaecd2743495cc316e545e5d6336ad8
ijlee2 <ijlee2@users.noreply.github.com>
2020-05-18 16:07:18 -0500

    Did prep work for the demo app

---
+++ tests/dummy/app/helpers/add.js
@@ -0,0 +1,7 @@
+import { helper } from '@ember/component/helper';
+
+export default helper(
+  function add(params) {
+    return params.reduce((accumulator, value) => accumulator + value, 0);
+  }
+);
