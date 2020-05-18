abd7d1218abb8cfdb1926a764df514a7b6b4143e
ijlee2 <ijlee2@users.noreply.github.com>
2020-05-17 21:56:25 -0500

    Added {{cq-width}} helper

---
+++ addon/helpers/cq-width.js
@@ -0,0 +1,10 @@
+import { helper } from '@ember/component/helper';
+
+export default helper(
+  function cqWidth(params, hash) {
+    const dimension = 'width';
+    const { min = 0, max = Infinity } = hash;
+
+    return { dimension, min, max };
+  }
+);
---
+++ app/helpers/cq-width.js
@@ -0,0 +1,1 @@
+export { default, cqWidth } from 'ember-container-query/helpers/cq-width';
