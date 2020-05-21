6755d4a7078ba4898a095e3901fe5fbf9e4f8561
ijlee2 <ijlee2@users.noreply.github.com>
2020-05-20 21:47:17 -0500

    Added Widget 1 for the Dashboard page

---
+++ tests/acceptance/dashboard-test.js
@@ -0,0 +1,118 @@
+import { visit } from '@ember/test-helpers';
+import takeSnapshot from 'dummy/tests/helpers/percy';
+import resetViewport from 'dummy/tests/helpers/reset-viewport';
+import { setupApplicationTest } from 'ember-qunit';
+import { module, test } from 'qunit';
+
+module('Acceptance | dashboard', function(hooks) {
+  setupApplicationTest(hooks);
+  resetViewport(hooks);
+
+
+  test('@w1 @h1 Visual snapshot', async function(assert) {
+    await visit('/dashboard');
+
+    assert.dom('[data-test-widget="1"] [data-test-container-query]')
+      .doesNotHaveAttribute('data-container-query-tall')
+      .doesNotHaveAttribute('data-container-query-square')
+      .hasAttribute('data-container-query-wide');
+
+    await takeSnapshot(assert);
+  });
+
+
+  test('@w2 @h1 Visual snapshot', async function(assert) {
+    await visit('/dashboard');
+
+    assert.dom('[data-test-widget="1"] [data-test-container-query]')
+      .doesNotHaveAttribute('data-container-query-tall')
+      .hasAttribute('data-container-query-square')
+      .doesNotHaveAttribute('data-container-query-wide');
+
+    await takeSnapshot(assert);
+  });
+
+
+  test('@w3 @h1 Visual snapshot', async function(assert) {
+    await visit('/dashboard');
+
+    assert.dom('[data-test-widget="1"] [data-test-container-query]')
+      .hasAttribute('data-container-query-tall')
+      .doesNotHaveAttribute('data-container-query-square')
+      .doesNotHaveAttribute('data-container-query-wide');
+
+    await takeSnapshot(assert);
+  });
+
+
+  test('@w1 @h2 Visual snapshot', async function(assert) {
+    await visit('/dashboard');
+
+    assert.dom('[data-test-widget="1"] [data-test-container-query]')
+      .doesNotHaveAttribute('data-container-query-tall')
+      .doesNotHaveAttribute('data-container-query-square')
+      .hasAttribute('data-container-query-wide');
+
+    await takeSnapshot(assert);
+  });
+
+
+  test('@w2 @h2 Visual snapshot', async function(assert) {
+    await visit('/dashboard');
+
+    assert.dom('[data-test-widget="1"] [data-test-container-query]')
+      .doesNotHaveAttribute('data-container-query-tall')
+      .hasAttribute('data-container-query-square')
+      .doesNotHaveAttribute('data-container-query-wide');
+
+    await takeSnapshot(assert);
+  });
+
+
+  test('@w3 @h2 Visual snapshot', async function(assert) {
+    await visit('/dashboard');
+
+    assert.dom('[data-test-widget="1"] [data-test-container-query]')
+      .hasAttribute('data-container-query-tall')
+      .doesNotHaveAttribute('data-container-query-square')
+      .doesNotHaveAttribute('data-container-query-wide');
+
+    await takeSnapshot(assert);
+  });
+
+
+  test('@w1 @h3 Visual snapshot', async function(assert) {
+    await visit('/dashboard');
+
+    assert.dom('[data-test-widget="1"] [data-test-container-query]')
+      .doesNotHaveAttribute('data-container-query-tall')
+      .doesNotHaveAttribute('data-container-query-square')
+      .hasAttribute('data-container-query-wide');
+
+    await takeSnapshot(assert);
+  });
+
+
+  test('@w2 @h3 Visual snapshot', async function(assert) {
+    await visit('/dashboard');
+
+    assert.dom('[data-test-widget="1"] [data-test-container-query]')
+      .doesNotHaveAttribute('data-container-query-tall')
+      .hasAttribute('data-container-query-square')
+      .doesNotHaveAttribute('data-container-query-wide');
+
+    await takeSnapshot(assert);
+  });
+
+
+  test('@w3 @h3 Visual snapshot', async function(assert) {
+    await visit('/dashboard');
+
+    assert.dom('[data-test-widget="1"] [data-test-container-query]')
+      .hasAttribute('data-container-query-tall')
+      .doesNotHaveAttribute('data-container-query-square')
+      .doesNotHaveAttribute('data-container-query-wide');
+
+    await takeSnapshot(assert);
+  });
+});
