49e534cff8e5d1cebf4106d10524527768a00a96
ijlee2 <ijlee2@users.noreply.github.com>
2020-05-23 23:52:02 -0500

    Added concert images and data

---
+++ tests/dummy/app/data/concert.js
@@ -0,0 +1,82 @@
+export default {
+  name: 'ACL Live',
+  date: 'Jun 01',
+  location: {
+    city: 'Austin',
+    state: 'TX'
+  },
+  images: [
+    {
+      path: 'images/widgets/widget-3',
+      fileName: 'venue-extra-wide@1x.jpg',
+      metadata: {
+        height: 150,
+        width: 540
+      }
+    },
+    {
+      path: 'images/widgets/widget-3',
+      fileName: 'venue-extra-wide@2x.jpg',
+      metadata: {
+        height: 300,
+        width: 1080
+      }
+    },
+    {
+      path: 'images/widgets/widget-3',
+      fileName: 'venue-extra-wide@4x.jpg',
+      metadata: {
+        height: 600,
+        width: 2160
+      }
+    },
+    {
+      path: 'images/widgets/widget-3',
+      fileName: 'venue-square@1x.jpg',
+      metadata: {
+        height: 150,
+        width: 150
+      }
+    },
+    {
+      path: 'images/widgets/widget-3',
+      fileName: 'venue-square@2x.jpg',
+      metadata: {
+        height: 300,
+        width: 300
+      }
+    },
+    {
+      path: 'images/widgets/widget-3',
+      fileName: 'venue-square@4x.jpg',
+      metadata: {
+        height: 600,
+        width: 600
+      }
+    },
+    {
+      path: 'images/widgets/widget-3',
+      fileName: 'venue-wide@1x.jpg',
+      metadata: {
+        height: 150,
+        width: 300
+      }
+    },
+    {
+      path: 'images/widgets/widget-3',
+      fileName: 'venue-wide@2x.jpg',
+      metadata: {
+        height: 300,
+        width: 600
+      }
+    },
+    {
+      path: 'images/widgets/widget-3',
+      fileName: 'venue-wide@4x.jpg',
+      metadata: {
+        height: 600,
+        width: 1200
+      }
+    }
+  ]
+};
