d2139ddd8406cc8477a4cee01970ac841548de4f
ijlee2 <ijlee2@users.noreply.github.com>
2020-08-12 07:52:51 -0500

    Upgraded ember-source to v3.20.4

--- tests/dummy/app/app.js
+++ tests/dummy/app/app.js
@@ -1,7 +1,7 @@
 import Application from '@ember/application';
+import config from 'dummy/config/environment';
 import Resolver from 'ember-resolver';
 import loadInitializers from 'ember-load-initializers';
-import config from './config/environment';
 
 export default class App extends Application {
   modulePrefix = config.modulePrefix;
