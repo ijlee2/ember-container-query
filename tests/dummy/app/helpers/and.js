3046060ae59df8dd43367f7176f0e9c7040b254f
Esteban Arango Medina <esteban.arango@envoy.com>
2020-10-26 18:13:23 -0500

    Delete custom helpers

--- tests/dummy/app/helpers/and.js
+++
@@ -1,7 +0,0 @@
-import { helper } from '@ember/component/helper';
-
-export default helper(
-  function and(params) {
-    return params[0] && params[1];
-  }
-);
--- tests/dummy/app/helpers/eq.js
+++
@@ -1,7 +0,0 @@
-import { helper } from '@ember/component/helper';
-
-export default helper(
-  function eq(params) {
-    return params[0] === params[1];
-  }
-);
