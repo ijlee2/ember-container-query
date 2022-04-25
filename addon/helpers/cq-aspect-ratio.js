77b6aef4fbdf2d2a5a8d5f629a6cd48548cead4b
ijlee2 <ijlee2@users.noreply.github.com>
2022-04-25 20:06:56 +0200

    Removed named export for the {{cq-aspect-ratio}} helper

--- addon/helpers/cq-aspect-ratio.js
+++ addon/helpers/cq-aspect-ratio.js
@@ -1,6 +1,6 @@
 import { helper } from '@ember/component/helper';
 
-export function cqAspectRatio(params, hash) {
+function cqAspectRatio(params, hash) {
   const dimension = 'aspectRatio';
   const { min = 0, max = Infinity } = hash;
 
