9139f87b7fef36b1d0837427100e98a22f5c2e08
Isaac Lee <16869656+ijlee2@users.noreply.github.com>
2022-04-25 17:49:49 +0200

    Added named exports for helpers (#111)
    * Added named export for the {{cq-aspect-ratio}} helper
    
    * Added named export for the {{cq-height}} helper
    
    * Added named export for the {{cq-width}} helper
    
    Co-authored-by: ijlee2 <ijlee2@users.noreply.github.com>

--- addon/helpers/cq-aspect-ratio.js
+++ addon/helpers/cq-aspect-ratio.js
@@ -1,8 +1,10 @@
 import { helper } from '@ember/component/helper';
 
-export default helper(function cqAspectRatio(params, hash) {
+export function cqAspectRatio(params, hash) {
   const dimension = 'aspectRatio';
   const { min = 0, max = Infinity } = hash;
 
   return { dimension, min, max };
-});
+}
+
+export default helper(cqAspectRatio);
--- addon/helpers/cq-width.js
+++ addon/helpers/cq-width.js
@@ -1,8 +1,10 @@
 import { helper } from '@ember/component/helper';
 
-export default helper(function cqWidth(params, hash) {
+export function cqWidth(params, hash) {
   const dimension = 'width';
   const { min = 0, max = Infinity } = hash;
 
   return { dimension, min, max };
-});
+}
+
+export default helper(cqWidth);
