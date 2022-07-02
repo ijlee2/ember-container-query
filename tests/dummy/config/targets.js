ac6b3e7b319a7df02c46a6eba1448ebebe53a3b1
ijlee2 <ijlee2@users.noreply.github.com>
2022-07-02 09:09:31 +0200

    chore: Installed stylelint

--- tests/dummy/config/targets.js
+++ tests/dummy/config/targets.js
@@ -3,10 +3,8 @@
 module.exports = {
   browsers: [
     'last 2 Chrome versions',
+    'last 2 Edge versions',
     'last 2 Firefox versions',
     'last 2 Safari versions',
-    // Latest versions of Microsoft Edge are built on top of Chromium.
-    // However, not a significant number of users use the latest yet.
-    'Edge >= 18',
   ],
 };
