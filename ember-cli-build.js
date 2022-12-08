7123383598e00eedda011c202e519583167a5319
Isaac Lee <16869656+ijlee2@users.noreply.github.com>
2022-12-08 21:48:32 +0100

    Remove ember-cli-typescript. Enable enableTypeScriptTransform instead. (#131)
    chore: Removed ember-cli-typescript. Enabled enableTypeScriptTransform instead
    
    Co-authored-by: ijlee2 <ijlee2@users.noreply.github.com>

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -6,6 +6,10 @@ const EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
 module.exports = function (defaults) {
   const app = new EmberAddon(defaults, {
     // Add options here
+    'ember-cli-babel': {
+      enableTypeScriptTransform: true,
+    },
+
     fingerprint: {
       exclude: ['images/'],
     },
