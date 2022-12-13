8a24f0a1811209bb2c740cf962c11009679209ca
ijlee2 <ijlee2@users.noreply.github.com>
2022-12-13 16:20:10 +0100

    feature: Added Glint signature to {{cq-width}} helper

--- addon/helpers/cq-width.ts
+++ addon/helpers/cq-width.ts
@@ -1,15 +1,23 @@
 import { helper } from '@ember/component/helper';
 import type { Metadata } from 'ember-container-query/modifiers/container-query';
 
-function cqWidth(
-  positional: unknown[],
-  named: Record<string, unknown>
-): Metadata {
-  const dimension = 'width';
-  const max = (named['max'] as number | undefined) ?? Infinity;
-  const min = (named['min'] as number | undefined) ?? 0;
-
-  return { dimension, max, min };
+interface CqWidthHelperSignature {
+  Args: {
+    Named: {
+      max?: number;
+      min?: number;
+    };
+    Positional: [];
+  };
+  Return: Metadata;
 }
 
-export default helper(cqWidth);
+const CqWidthHelper = helper<CqWidthHelperSignature>((_positional, named) => {
+  const dimension = 'width';
+  const max = named.max ?? Infinity;
+  const min = named.min ?? 0;
+
+  return { dimension, max, min };
+});
+
+export default CqWidthHelper;
