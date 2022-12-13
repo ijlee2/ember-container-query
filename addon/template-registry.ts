e7fab31dc1e5dbb5b3f6580058db5a93fe56ab2e
ijlee2 <ijlee2@users.noreply.github.com>
2022-12-13 16:21:48 +0100

    feature: Added Glint signature to {{container-query}} modifier

--- addon/template-registry.ts
+++ addon/template-registry.ts
@@ -2,9 +2,11 @@ import type ContainerQueryComponent from 'ember-container-query/components/conta
 import type CqAspectRatioHelper from 'ember-container-query/helpers/cq-aspect-ratio';
 import type CqHeightHelper from 'ember-container-query/helpers/cq-height';
 import type CqWidthHelper from 'ember-container-query/helpers/cq-width';
+import type ContainerQueryModifier from 'ember-container-query/modifiers/container-query';
 
 export default interface EmberContainerQueryRegistry {
   ContainerQuery: typeof ContainerQueryComponent;
+  'container-query': typeof ContainerQueryModifier;
   'cq-aspect-ratio': typeof CqAspectRatioHelper;
   'cq-height': typeof CqHeightHelper;
   'cq-width': typeof CqWidthHelper;
