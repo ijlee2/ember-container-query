b6970803e499aee944071ea369ea9905ec01b39a
ijlee2 <ijlee2@users.noreply.github.com>
2022-12-13 16:18:58 +0100

    feature: Added Glint signature to {{cq-height}} helper

--- addon/template-registry.ts
+++ addon/template-registry.ts
@@ -1,7 +1,9 @@
 import type ContainerQueryComponent from 'ember-container-query/components/container-query';
 import type CqAspectRatioHelper from 'ember-container-query/helpers/cq-aspect-ratio';
+import type CqHeightHelper from 'ember-container-query/helpers/cq-height';
 
 export default interface EmberContainerQueryRegistry {
   ContainerQuery: typeof ContainerQueryComponent;
   'cq-aspect-ratio': typeof CqAspectRatioHelper;
+  'cq-height': typeof CqHeightHelper;
 }
