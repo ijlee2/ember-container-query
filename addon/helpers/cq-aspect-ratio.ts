5f610fe239085805db405ff68f3aeaa06d5dabf0
ijlee2 <ijlee2@users.noreply.github.com>
2022-12-13 16:15:49 +0100

    feature: Added Glint signature to {{cq-aspect-ratio}} helper

--- addon/helpers/cq-aspect-ratio.ts
+++ addon/helpers/cq-aspect-ratio.ts
@@ -1,15 +1,25 @@
 import { helper } from '@ember/component/helper';
 import type { Metadata } from 'ember-container-query/modifiers/container-query';
 
-function cqAspectRatio(
-  positional: unknown[],
-  named: Record<string, unknown>
-): Metadata {
-  const dimension = 'aspectRatio';
-  const max = (named['max'] as number | undefined) ?? Infinity;
-  const min = (named['min'] as number | undefined) ?? 0;
-
-  return { dimension, max, min };
+interface CqAspectRatioHelperSignature {
+  Args: {
+    Named: {
+      max?: number;
+      min?: number;
+    };
+    Positional: [];
+  };
+  Return: Metadata;
 }
 
-export default helper(cqAspectRatio);
+const CqAspectRatioHelper = helper<CqAspectRatioHelperSignature>(
+  (_positional, named) => {
+    const dimension = 'aspectRatio';
+    const max = named.max ?? Infinity;
+    const min = named.min ?? 0;
+
+    return { dimension, max, min };
+  }
+);
+
+export default CqAspectRatioHelper;
--- addon/template-registry.ts
+++ addon/template-registry.ts
@@ -1,5 +1,7 @@
 import type ContainerQueryComponent from 'ember-container-query/components/container-query';
+import type CqAspectRatioHelper from 'ember-container-query/helpers/cq-aspect-ratio';
 
 export default interface EmberContainerQueryRegistry {
   ContainerQuery: typeof ContainerQueryComponent;
+  'cq-aspect-ratio': typeof CqAspectRatioHelper;
 }
