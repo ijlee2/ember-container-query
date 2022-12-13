241f4516f947cff555d66f98e9e027cf7a1b1852
ijlee2 <ijlee2@users.noreply.github.com>
2022-12-13 16:18:58 +0100

    feature: Added Glint signature to {{cq-height}} helper

--- addon/helpers/cq-height.ts
+++ addon/helpers/cq-height.ts
@@ -1,15 +1,23 @@
 import { helper } from '@ember/component/helper';
 import type { Metadata } from 'ember-container-query/modifiers/container-query';
 
-function cqHeight(
-  positional: unknown[],
-  named: Record<string, unknown>
-): Metadata {
-  const dimension = 'height';
-  const max = (named['max'] as number | undefined) ?? Infinity;
-  const min = (named['min'] as number | undefined) ?? 0;
-
-  return { dimension, max, min };
+interface CqHeightHelperSignature {
+  Args: {
+    Named: {
+      max?: number;
+      min?: number;
+    };
+    Positional: [];
+  };
+  Return: Metadata;
 }
 
-export default helper(cqHeight);
+const CqHeightHelper = helper<CqHeightHelperSignature>((_positional, named) => {
+  const dimension = 'height';
+  const max = named.max ?? Infinity;
+  const min = named.min ?? 0;
+
+  return { dimension, max, min };
+});
+
+export default CqHeightHelper;
