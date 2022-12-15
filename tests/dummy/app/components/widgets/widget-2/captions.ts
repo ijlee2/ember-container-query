a80ab2c5062f48ca59ff5407cb3ac4cf02defff5
ijlee2 <ijlee2@users.noreply.github.com>
2022-12-15 16:01:47 +0100

    chore: Added Glint signature to <Widgets::Widget-2> components

--- tests/dummy/app/components/widgets/widget-2/captions.ts
+++ tests/dummy/app/components/widgets/widget-2/captions.ts
@@ -3,11 +3,13 @@ import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
 import type { Summary } from 'dummy/utils/components/widgets/widget-2';
 
-interface WidgetsWidget2CaptionsComponentArgs {
-  summaries?: Array<Summary>;
+interface WidgetsWidget2CaptionsComponentSignature {
+  Args: {
+    summaries?: Array<Summary>;
+  };
 }
 
-export default class WidgetsWidget2CaptionsComponent extends Component<WidgetsWidget2CaptionsComponentArgs> {
+export default class WidgetsWidget2CaptionsComponent extends Component<WidgetsWidget2CaptionsComponentSignature> {
   @tracked currentIndex = 0;
 
   get canShowNextButton(): boolean {
@@ -33,5 +35,11 @@ export default class WidgetsWidget2CaptionsComponent extends Component<WidgetsWi
     const nextIndex = (currentIndex + increment + numSummaries) % numSummaries;
 
     this.currentIndex = nextIndex;
+  }
+}
+
+declare module '@glint/environment-ember-loose/registry' {
+  export default interface Registry {
+    'Widgets::Widget-2::Captions': typeof WidgetsWidget2CaptionsComponent;
   }
 }
