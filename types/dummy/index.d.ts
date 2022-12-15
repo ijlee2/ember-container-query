69a71477d78c558090dc1190eeb7630aadb9fdb9
ijlee2 <ijlee2@users.noreply.github.com>
2022-12-15 15:33:27 +0100

    chore: Listed the registry entries of the demo app's dependencies

--- types/dummy/index.d.ts
+++ types/dummy/index.d.ts
@@ -3,13 +3,31 @@
 
 import '@glint/environment-ember-loose';
 
+import SvgJarHelper from '@gavant/glint-template-types/types/ember-svg-jar/helpers/svg-jar';
+import AndHelper from '@gavant/glint-template-types/types/ember-truth-helpers/helpers/and';
+import OrHelper from '@gavant/glint-template-types/types/ember-truth-helpers/helpers/or';
+import { ComponentLike, HelperLike } from '@glint/template';
 import type EmberContainerQueryRegistry from 'ember-container-query/template-registry';
 
+type NavigationNarratorComponent = ComponentLike<{
+  Args: {
+    skipTo: string;
+  };
+}>;
+
+type PageTitleHelper = HelperLike<{
+  Args: { Positional: [title: string] };
+  Return: void;
+}>;
+
 declare module '@glint/environment-ember-loose/registry' {
-  // Remove this once entries have been added! 👇
-  // eslint-disable-next-line @typescript-eslint/no-empty-interface
   export default interface Registry extends EmberContainerQueryRegistry {
     // Add any registry entries from other addons here that your addon itself uses (in non-strict mode templates)
     // See https://typed-ember.gitbook.io/glint/using-glint/ember/using-addons
+    NavigationNarrator: NavigationNarratorComponent;
+    and: typeof AndHelper;
+    or: typeof OrHelper;
+    'page-title': PageTitleHelper;
+    'svg-jar': typeof SvgJarHelper;
   }
 }
