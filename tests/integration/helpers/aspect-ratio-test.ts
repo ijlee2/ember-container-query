70db95195b01b9f2212ca2b6073b5d6b0972ca87
ijlee2 <ijlee2@users.noreply.github.com>
2023-01-03 11:09:04 +0100

    chore: Renamed the helpers by dropping the cq prefix

--- tests/integration/helpers/aspect-ratio-test.ts
+++ tests/integration/helpers/aspect-ratio-test.ts
@@ -4,34 +4,34 @@ import { setupRenderingTest } from 'dummy/tests/helpers';
 import { hbs } from 'ember-cli-htmlbars';
 import { module, test } from 'qunit';
 
-module('Integration | Helper | cq-height', function (hooks) {
+module('Integration | Helper | aspect-ratio', function (hooks) {
   setupRenderingTest(hooks);
 
   test('can return a hash with default values', async function (this: TestContext, assert) {
     await render<TestContext>(hbs`
-      {{#let (cq-height) as |output|}}
+      {{#let (aspect-ratio) as |output|}}
         <p data-test-value="dimension">{{output.dimension}}</p>
         <p data-test-value="min">{{output.min}}</p>
         <p data-test-value="max">{{output.max}}</p>
       {{/let}}
     `);
 
-    assert.dom('[data-test-value="dimension"]').hasText('height');
+    assert.dom('[data-test-value="dimension"]').hasText('aspectRatio');
     assert.dom('[data-test-value="min"]').hasText('0');
     assert.dom('[data-test-value="max"]').hasText('Infinity');
   });
 
   test('if min and max are provided, returns them as they are', async function (this: TestContext, assert) {
     await render<TestContext>(hbs`
-      {{#let (cq-height min=100 max=200) as |output|}}
+      {{#let (aspect-ratio min=0.25 max=0.75) as |output|}}
         <p data-test-value="dimension">{{output.dimension}}</p>
         <p data-test-value="min">{{output.min}}</p>
         <p data-test-value="max">{{output.max}}</p>
       {{/let}}
     `);
 
-    assert.dom('[data-test-value="dimension"]').hasText('height');
-    assert.dom('[data-test-value="min"]').hasText('100');
-    assert.dom('[data-test-value="max"]').hasText('200');
+    assert.dom('[data-test-value="dimension"]').hasText('aspectRatio');
+    assert.dom('[data-test-value="min"]').hasText('0.25');
+    assert.dom('[data-test-value="max"]').hasText('0.75');
   });
 });
