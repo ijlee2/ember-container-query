4999f2c1cf23d07181486b5a566823a31995b9cc
NullVoxPopuli <LPSego3+dev@gmail.com>
2023-01-04 09:44:07 -0500

    Add easier imports from strict mode users (#130)
    * Add easier imports from strict mode users
    
    * Update root re-exports
    
    * Reduce noise in pull request

---
+++ addon/index.ts
@@ -0,0 +1,5 @@
+export { default as ContainerQuery } from './components/container-query';
+export { default as aspectRatio } from './helpers/aspect-ratio';
+export { default as height } from './helpers/height';
+export { default as width } from './helpers/width';
+export { default as containerQuery } from './modifiers/container-query';
