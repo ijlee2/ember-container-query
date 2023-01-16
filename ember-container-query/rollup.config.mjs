594f577b41a148fad632e297f484705f450f58e2
ijlee2 <ijlee2@users.noreply.github.com>
2023-01-16 11:49:57 +0100

    chore: Ran the lint:fix script (addon)

--- ember-container-query/rollup.config.mjs
+++ ember-container-query/rollup.config.mjs
@@ -1,6 +1,6 @@
-import typescript from 'rollup-plugin-ts';
-import copy from 'rollup-plugin-copy';
 import { Addon } from '@embroider/addon-dev/rollup';
+import copy from 'rollup-plugin-copy';
+import typescript from 'rollup-plugin-ts';
 
 const addon = new Addon({
   srcDir: 'src',
@@ -15,12 +15,32 @@ export default {
   plugins: [
     // These are the modules that users should be able to import from your
     // addon. Anything not listed here may get optimized away.
-    addon.publicEntrypoints(['components/container-query.js', 'helpers/aspect-ratio.js', 'helpers/cq-aspect-ratio.js', 'helpers/cq-height.js', 'helpers/cq-width.js', 'helpers/height.js', 'helpers/width.js', 'index.js', 'modifiers/container-query.js', 'template-registry.js']),
+    addon.publicEntrypoints([
+      'components/container-query.js',
+      'helpers/aspect-ratio.js',
+      'helpers/cq-aspect-ratio.js',
+      'helpers/cq-height.js',
+      'helpers/cq-width.js',
+      'helpers/height.js',
+      'helpers/width.js',
+      'index.js',
+      'modifiers/container-query.js',
+      'template-registry.js',
+    ]),
 
     // These are the modules that should get reexported into the traditional
     // "app" tree. Things in here should also be in publicEntrypoints above, but
     // not everything in publicEntrypoints necessarily needs to go here.
-    addon.appReexports(['components/container-query.js', 'helpers/aspect-ratio.js', 'helpers/cq-aspect-ratio.js', 'helpers/cq-height.js', 'helpers/cq-width.js', 'helpers/height.js', 'helpers/width.js', 'modifiers/container-query.js']),
+    addon.appReexports([
+      'components/container-query.js',
+      'helpers/aspect-ratio.js',
+      'helpers/cq-aspect-ratio.js',
+      'helpers/cq-height.js',
+      'helpers/cq-width.js',
+      'helpers/height.js',
+      'helpers/width.js',
+      'modifiers/container-query.js',
+    ]),
 
     // Follow the V2 Addon rules about dependencies. Your code can import from
     // `dependencies` and `peerDependencies` as well as standard Ember-provided
