7c4ebdd0e2beb0ed9e91c0e95ae0a4ce2bf8d83d
ijlee2 <ijlee2@users.noreply.github.com>
2023-03-08 11:22:59 +0100

    feature: Enabled stricter Embroider settings and route splitting (docs-app)

--- docs-app/ember-cli-build.js
+++ docs-app/ember-cli-build.js
@@ -12,10 +12,21 @@ module.exports = function (defaults) {
   });
 
   return require('@embroider/compat').compatBuild(app, Webpack, {
+    packagerOptions: {
+      webpackConfig: {
+        // ...
+      },
+    },
     skipBabel: [
       {
         package: 'qunit',
       },
     ],
+    splitAtRoutes: ['album', 'dashboard', 'form', 'products'],
+    staticAddonTestSupportTrees: true,
+    staticAddonTrees: false, // due to ember-css-modules
+    staticComponents: false, // due to ember-css-modules
+    staticHelpers: true,
+    staticModifiers: true,
   });
 };
