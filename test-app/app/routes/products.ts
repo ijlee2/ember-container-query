afa9299c6b44fbdd4ee6d1c3f95ceb98cad54f3a
ijlee2 <ijlee2@users.noreply.github.com>
2023-03-08 10:44:41 +0100

    chore: Removed irrelevant code (test-app)

--- test-app/app/routes/products.ts
+++
@@ -1,9 +0,0 @@
-import Route from '@ember/routing/route';
-
-import products from '../data/products';
-
-export default class ProductsRoute extends Route {
-  model() {
-    return products;
-  }
-}
--- test-app/tests/acceptance/index/accessibility-test.ts
+++ test-app/tests/acceptance/index/accessibility-test.ts
@@ -7,60 +7,10 @@ import { setupApplicationTest, timeout } from '../../helpers';
 module('Acceptance | index', function (hooks) {
   setupApplicationTest(hooks);
 
-  hooks.beforeEach(async function () {
+  test('Accessibility audit', async function (assert) {
     await visit('/');
     await timeout();
-  });
 
-  test('@w1 @h1 Accessibility audit', async function (assert) {
-    await a11yAudit();
-
-    assert.ok(true, 'We passed Axe tests.');
-  });
-
-  test('@w2 @h1 Accessibility audit', async function (assert) {
-    await a11yAudit();
-
-    assert.ok(true, 'We passed Axe tests.');
-  });
-
-  test('@w3 @h1 Accessibility audit', async function (assert) {
-    await a11yAudit();
-
-    assert.ok(true, 'We passed Axe tests.');
-  });
-
-  test('@w1 @h2 Accessibility audit', async function (assert) {
-    await a11yAudit();
-
-    assert.ok(true, 'We passed Axe tests.');
-  });
-
-  test('@w2 @h2 Accessibility audit', async function (assert) {
-    await a11yAudit();
-
-    assert.ok(true, 'We passed Axe tests.');
-  });
-
-  test('@w3 @h2 Accessibility audit', async function (assert) {
-    await a11yAudit();
-
-    assert.ok(true, 'We passed Axe tests.');
-  });
-
-  test('@w1 @h3 Accessibility audit', async function (assert) {
-    await a11yAudit();
-
-    assert.ok(true, 'We passed Axe tests.');
-  });
-
-  test('@w2 @h3 Accessibility audit', async function (assert) {
-    await a11yAudit();
-
-    assert.ok(true, 'We passed Axe tests.');
-  });
-
-  test('@w3 @h3 Accessibility audit', async function (assert) {
     await a11yAudit();
 
     assert.ok(true, 'We passed Axe tests.');
