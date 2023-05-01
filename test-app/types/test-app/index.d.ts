80681188942e69d821687315bbf00ccf13d997c5
ijlee2 <ijlee2@users.noreply.github.com>
2023-05-01 08:05:20 +0200

    chore: Updated development dependencies

--- test-app/types/test-app/index.d.ts
+++ test-app/types/test-app/index.d.ts
@@ -2,6 +2,7 @@
 // These will *not* be published as part of your addon, so be careful that your published code does not rely on them!
 
 import '@glint/environment-ember-loose';
+import '@glint/environment-ember-template-imports';
 
 import type EmberContainerQueryRegistry from 'ember-container-query/template-registry';
 
