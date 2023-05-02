0ab7efd7ff306b9425814dc19a38a26c7a4ec5d3
ijlee2 <ijlee2@users.noreply.github.com>
2023-05-02 13:47:04 +0200

    chore: Migrated to pnpm and installed missing development dependencies

--- test-app/types/test-app/index.d.ts
+++ test-app/types/test-app/index.d.ts
@@ -1,8 +1,7 @@
-// Add any types here that you need for local development only.
-// These will *not* be published as part of your addon, so be careful that your published code does not rely on them!
-
 import '@glint/environment-ember-loose';
 import '@glint/environment-ember-template-imports';
+import 'ember-source/types';
+import 'ember-source/types/preview';
 
 import type EmberContainerQueryRegistry from 'ember-container-query/template-registry';
 
