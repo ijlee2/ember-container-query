9a1d91428415750edc73589221a49d1f979257e5
ijlee2 <ijlee2@users.noreply.github.com>
2023-07-05 11:05:30 +0200

    chore: Reverted committing declarations and dist

--- ember-container-query/dist/index.js
+++
@@ -1,6 +0,0 @@
-export { default as ContainerQuery } from './components/container-query.js';
-export { default as aspectRatio } from './helpers/aspect-ratio.js';
-export { default as height } from './helpers/height.js';
-export { default as width } from './helpers/width.js';
-export { default as containerQuery } from './modifiers/container-query.js';
-//# sourceMappingURL=index.js.map
