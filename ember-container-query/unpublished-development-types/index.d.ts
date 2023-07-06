f81d4f6fbddb6364c1238d8171786d4a5cb3fd01
Isaac Lee <16869656+ijlee2@users.noreply.github.com>
2023-07-06 08:11:33 +0200

    Remove rollup-plugin-ts (#203)
    * chore: Temporarily committed dist
    
    * chore: Removed rollup-plugin-ts
    
    * chore: Temporarily committed declarations and dist
    
    * bugfix: Removed .hbs from extensions
    
    * chore: Committed dist
    
    * chore: Reverted committing declarations and dist
    
    * bug: Use glint -d
    
    * Revert "bug: Use glint -d"
    
    This reverts commit ccc9fe01e741ad0ecb04e53f7efead163bbd2288.
    
    * bugfix: Generalized scripts and TS configurations (works for both tsc and glint)
    
    * Revert "bugfix: Generalized scripts and TS configurations (works for both tsc and glint)"
    
    This reverts commit ae9e11e2a1e52c05971c88b677f1dac79220ebbe.
    
    * Revert "Revert "bug: Use glint -d""
    
    This reverts commit 0ad720063f2c8b7580c05c67a359a562e271c5b3.
    
    * bugfix: Removed @glint-ignore comment, which had caused TypeScript to error when noEmitOnError is set to true
    
    ---------
    
    Co-authored-by: ijlee2 <ijlee2@users.noreply.github.com>

--- ember-container-query/unpublished-development-types/index.d.ts
+++ ember-container-query/unpublished-development-types/index.d.ts
@@ -3,11 +3,26 @@
 
 import '@glint/environment-ember-loose';
 
+import { ComponentLike, HelperLike } from '@glint/template';
+
 import EmberContainerQueryRegistry from '../src/template-registry';
+
+interface ElementHelperSignature<T extends string> {
+  Args: {
+    Positional: [name: T];
+  };
+  Return: ComponentLike<{
+    Blocks: { default: [] };
+    Element: T extends keyof HTMLElementTagNameMap
+      ? HTMLElementTagNameMap[T]
+      : Element;
+  }>;
+}
 
 declare module '@glint/environment-ember-loose/registry' {
   export default interface Registry extends EmberContainerQueryRegistry {
     // Add any registry entries from other addons here that your addon itself uses (in non-strict mode templates)
     // See https://typed-ember.gitbook.io/glint/using-glint/ember/using-addons
+    element: HelperLike<ElementHelperSignature>;
   }
 }
