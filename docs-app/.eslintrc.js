58faea5f90e7670a9a62296f4383991373be843f
ijlee2 <ijlee2@users.noreply.github.com>
2023-12-18 19:44:49 +0100

    chore: Reconfigured eslint

--- docs-app/.eslintrc.js
+++ docs-app/.eslintrc.js
@@ -4,4 +4,13 @@ require('@shared-configs/eslint-config-ember/patch');
 
 module.exports = {
   extends: ['@shared-configs/eslint-config-ember/app'],
+  overrides: [
+    // TypeScript files
+    {
+      files: ['**/*.{gts,ts}'],
+      rules: {
+        '@typescript-eslint/no-explicit-any': 'off',
+      },
+    },
+  ],
 };
