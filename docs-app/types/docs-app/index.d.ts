d85b49b950106d0954e4636226a308be87ef3d1d
ijlee2 <ijlee2@users.noreply.github.com>
2024-02-19 16:05:17 +0100

    chore: Consumed the template registry from ember-page-title

--- docs-app/types/docs-app/index.d.ts
+++ docs-app/types/docs-app/index.d.ts
@@ -6,8 +6,9 @@ import '@glint/environment-ember-template-imports';
 import 'ember-source/types';
 import 'ember-source/types/preview';
 
-import type { ComponentLike, HelperLike } from '@glint/template';
+import type { ComponentLike } from '@glint/template';
 import type EmberContainerQueryRegistry from 'ember-container-query/template-registry';
+import type EmberPageTitleRegistry from 'ember-page-title/template-registry';
 import type EmberSvgJarRegistry from 'ember-svg-jar/template-registry';
 import type EmberTruthHelpersRegistry from 'ember-truth-helpers/template-registry';
 import type EmbroiderCssModulesRegistry from 'embroider-css-modules/template-registry';
@@ -18,20 +19,15 @@ type NavigationNarratorComponent = ComponentLike<{
   };
 }>;
 
-type PageTitleHelper = HelperLike<{
-  Args: { Positional: [title: string] };
-  Return: void;
-}>;
-
 declare module '@glint/environment-ember-loose/registry' {
   export default interface Registry
     extends EmberContainerQueryRegistry,
+      EmberPageTitleRegistry,
       EmberSvgJarRegistry,
       EmberTruthHelpersRegistry,
       EmbroiderCssModulesRegistry {
     // Add any registry entries from other addons here that your addon itself uses (in non-strict mode templates)
     // See https://typed-ember.gitbook.io/glint/using-glint/ember/using-addons
     NavigationNarrator: NavigationNarratorComponent;
-    'page-title': PageTitleHelper;
   }
 }
