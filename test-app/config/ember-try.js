86acb445ce0f3cfc2773cd1436fe37b71d1072c5
Isaac Lee <16869656+ijlee2@users.noreply.github.com>
2024-06-07 10:04:07 +0200

    Replaced ember-source-channel-url with latest-version. Added ember-lts-5.8 scenario. (#230)
    * chore: Replaced ember-source-channel-url with latest-version. Added ember-lts-5.8 scenario.
    
    * chore: Added lockfile
    
    ---------
    
    Co-authored-by: ijlee2 <ijlee2@users.noreply.github.com>

--- test-app/config/ember-try.js
+++ test-app/config/ember-try.js
@@ -1,9 +1,10 @@
 'use strict';
 
-const getChannelURL = require('ember-source-channel-url');
 const { embroiderOptimized, embroiderSafe } = require('@embroider/test-setup');
 
 module.exports = async function () {
+  const { default: latestVersion } = await import('latest-version');
+
   return {
     usePnpm: true,
     scenarios: [
@@ -45,10 +46,18 @@ module.exports = async function () {
         },
       },
       {
+        name: 'ember-lts-5.8',
+        npm: {
+          devDependencies: {
+            'ember-source': '~5.8.0',
+          },
+        },
+      },
+      {
         name: 'ember-release',
         npm: {
           devDependencies: {
-            'ember-source': await getChannelURL('release'),
+            'ember-source': await latestVersion('ember-source'),
           },
         },
       },
@@ -56,8 +65,9 @@ module.exports = async function () {
         name: 'ember-beta',
         npm: {
           devDependencies: {
-            // 'ember-source': await getChannelURL('beta'),
-            'ember-source': '5.9.0-beta.1',
+            'ember-source': await latestVersion('ember-source', {
+              version: 'beta',
+            }),
           },
         },
       },
@@ -65,8 +75,9 @@ module.exports = async function () {
         name: 'ember-canary',
         npm: {
           devDependencies: {
-            // 'ember-source': await getChannelURL('canary'),
-            'ember-source': '5.10.0-alpha.1',
+            'ember-source': await latestVersion('ember-source', {
+              version: 'alpha',
+            }),
           },
         },
       },
