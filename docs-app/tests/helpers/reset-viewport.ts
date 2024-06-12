ebe339d06b2c579a6157eebfca3f378ff2136484
ijlee2 <ijlee2@users.noreply.github.com>
2024-06-12 08:28:08 +0200

    chore: Updated class name

--- docs-app/tests/helpers/reset-viewport.ts
+++ docs-app/tests/helpers/reset-viewport.ts
@@ -2,7 +2,7 @@
   Do not allow ember-qunit to resize the viewport! Resizing the viewport causes
   issues with tests and Percy snapshots for this addon.
 
-  https://github.com/emberjs/ember-qunit/blob/master/vendor/ember-qunit/test-container-styles.css
+  https://github.com/emberjs/ember-qunit/blob/v8.1.0/addon/src/test-container-styles.css#L33
 */
 export function resetViewport(hooks: NestedHooks): void {
   hooks.beforeEach(function () {
@@ -10,6 +10,6 @@ export function resetViewport(hooks: NestedHooks): void {
       'ember-testing-container',
     )!;
 
-    testingContainer.classList.add('full-screen');
+    testingContainer.classList.add('ember-testing-container-full-screen');
   });
 }
