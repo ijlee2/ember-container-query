dd25a82c1b6439f1fed6c9bab8148ce3aeab9206
ijlee2 <ijlee2@users.noreply.github.com>
2024-08-13 12:52:51 +0200

    Revert "chore: Temporarily committed declarations"
    This reverts commit 2aadd319454349a1805a9963a58163142ba6fd22.

--- packages/ember-container-query/declarations/index.d.ts
+++
@@ -1,7 +0,0 @@
-export { default as ContainerQuery } from './components/container-query.ts';
-export { default as aspectRatio } from './helpers/aspect-ratio.ts';
-export { default as height } from './helpers/height.ts';
-export { default as width } from './helpers/width.ts';
-export type { Dimensions, Features, IndexSignatureParameter, Metadata, QueryResults, } from './modifiers/container-query.ts';
-export { default as containerQuery } from './modifiers/container-query.ts';
-//# sourceMappingURL=index.d.ts.map
