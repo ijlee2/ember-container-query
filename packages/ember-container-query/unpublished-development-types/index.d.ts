e4cd23723abab3a85af677ebce9234a5ff8ab111
ijlee2 <ijlee2@users.noreply.github.com>
2024-08-13 12:51:25 +0200

    refactor: Replaced @types/ember__* with native types

--- packages/ember-container-query/unpublished-development-types/index.d.ts
+++ packages/ember-container-query/unpublished-development-types/index.d.ts
@@ -3,6 +3,7 @@
 
 import '@glint/environment-ember-loose';
 import '@glint/environment-ember-template-imports';
+import 'ember-source/types';
 
 import type EmberElementHelperRegistry from 'ember-element-helper/template-registry';
 
