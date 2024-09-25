33c1ba5d0c6397fe5da1d76a3315060b3d145b44
Robbie Wagner <rwwagner90@gmail.com>
2024-09-25 15:50:13 -0400

    Update template-registry.ts

--- packages/ember-container-query/src/template-registry.ts
+++ packages/ember-container-query/src/template-registry.ts
@@ -4,7 +4,7 @@ import type HeightHelper from './helpers/height.ts';
 import type WidthHelper from './helpers/width.ts';
 import type ContainerQueryModifier from './modifiers/container-query.ts';
 
-export default interface EmberContainerQueryRegistry {
+export default interface Registry {
   ContainerQuery: typeof ContainerQueryComponent;
   'aspect-ratio': typeof AspectRatioHelper;
   'container-query': typeof ContainerQueryModifier;
