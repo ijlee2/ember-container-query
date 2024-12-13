3933b0ac9bc7a91c70cbea89f005ef5a668d60f6
ijlee2 <ijlee2@users.noreply.github.com>
2024-12-13 08:26:40 +0100

    chore: Updated ember-source to 6.0.1

--- test-app/types/test-app/index.d.ts
+++ test-app/types/test-app/index.d.ts
@@ -1,7 +1,6 @@
 import '@glint/environment-ember-loose';
 import '@glint/environment-ember-template-imports';
 import 'ember-source/types';
-import 'ember-source/types/preview';
 
 import type EmberContainerQueryRegistry from 'ember-container-query/template-registry';
 import type EmberPageTitleRegistry from 'ember-page-title/template-registry';
