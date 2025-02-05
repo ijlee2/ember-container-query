605c6ba3888785b6da8eadaf242acdabe71aa5c6
ijlee2 <ijlee2@users.noreply.github.com>
2025-02-05 10:59:13 +0100

    chore: Updated shared eslint configurations

--- configs/eslint/node/typescript/index.js
+++ configs/eslint/node/typescript/index.js
@@ -1,63 +1,121 @@
-'use strict';
+import babelEslintParser from '@babel/eslint-parser';
+import eslint from '@eslint/js';
+import eslintPluginImport from 'eslint-plugin-import';
+import eslintPluginN from 'eslint-plugin-n';
+import eslintPluginPrettier from 'eslint-plugin-prettier/recommended';
+import eslintPluginSimpleImportSort from 'eslint-plugin-simple-import-sort';
+import eslintPluginTypescriptSortKeys from 'eslint-plugin-typescript-sort-keys';
+import globals from 'globals';
+// eslint-disable-next-line import/no-unresolved
+import tseslint from 'typescript-eslint';
 
-module.exports = {
-  root: true,
-  parserOptions: {
-    ecmaVersion: 'latest',
-    project: true,
-    sourceType: 'module',
-  },
-  plugins: ['simple-import-sort'],
-  extends: [
-    'eslint:recommended',
-    'plugin:import/recommended',
-    'plugin:n/recommended',
-    'plugin:prettier/recommended',
-  ],
-  rules: {
-    curly: 'error',
-    'simple-import-sort/exports': 'error',
-    'simple-import-sort/imports': 'error',
-  },
-  settings: {
-    'import/resolver': {
-      node: true,
-      typescript: true,
-    },
-  },
-  overrides: [
-    // TypeScript files
-    {
-      files: ['**/*.{cts,ts}'],
-      parser: '@typescript-eslint/parser',
-      extends: [
-        'plugin:@typescript-eslint/recommended',
-        'plugin:import/typescript',
-        'plugin:typescript-sort-keys/recommended',
+const parserOptionsJs = {
+  babelOptions: {
+    plugins: [
+      [
+        '@babel/plugin-proposal-decorators',
+        {
+          decoratorsBeforeExport: true,
+        },
       ],
-      rules: {
-        '@typescript-eslint/array-type': 'error',
-        '@typescript-eslint/consistent-type-exports': 'error',
-        '@typescript-eslint/consistent-type-imports': 'error',
-        '@typescript-eslint/no-import-type-side-effects': 'error',
-        'import/no-duplicates': 'error',
-      },
-    },
-    // JavaScript files
-    {
-      files: ['**/*.{cjs,js}'],
-      rules: {
-        'import/no-duplicates': 'error',
-      },
-    },
-    // Node files
-    {
-      files: ['./.eslintrc.{cjs,js}', './.prettierrc.{cjs,js}'],
-      env: {
-        browser: false,
-        node: true,
-      },
-      extends: ['plugin:n/recommended'],
-    },
-  ],
+    ],
+  },
+  ecmaFeatures: {
+    modules: true,
+  },
+  ecmaVersion: 'latest',
+  requireConfigFile: false,
 };
+
+const parserOptionsTs = {
+  projectService: true,
+  tsconfigRootDir: import.meta.dirname,
+};
+
+export default tseslint.config(
+  {
+    linterOptions: {
+      reportUnusedDisableDirectives: 'error',
+    },
+  },
+  {
+    plugins: {
+      'simple-import-sort': eslintPluginSimpleImportSort,
+    },
+    rules: {
+      curly: 'error',
+      'simple-import-sort/imports': 'error',
+      'simple-import-sort/exports': 'error',
+    },
+  },
+
+  eslint.configs.recommended,
+  eslintPluginImport.flatConfigs.recommended,
+  eslintPluginPrettier,
+
+  // JavaScript files
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      parser: babelEslintParser,
+      parserOptions: parserOptionsJs,
+    },
+    rules: {
+      'import/no-duplicates': 'error',
+    },
+  },
+
+  // TypeScript files
+  {
+    extends: [
+      ...tseslint.configs.recommended,
+      eslintPluginImport.flatConfigs.typescript,
+    ],
+    files: ['**/*.ts'],
+    languageOptions: {
+      parserOptions: parserOptionsTs,
+    },
+    plugins: {
+      'typescript-sort-keys': eslintPluginTypescriptSortKeys,
+    },
+    rules: {
+      '@typescript-eslint/array-type': 'error',
+      '@typescript-eslint/consistent-type-imports': 'error',
+      '@typescript-eslint/no-import-type-side-effects': 'error',
+      'import/no-duplicates': 'error',
+      'typescript-sort-keys/interface': 'error',
+      'typescript-sort-keys/string-enum': 'error',
+    },
+    settings: {
+      'import/resolver': {
+        node: true,
+        typescript: true,
+      },
+    },
+  },
+
+  // Configuration files
+  {
+    files: ['**/*.cjs'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      globals: globals.node,
+      sourceType: 'script',
+    },
+    plugins: {
+      n: eslintPluginN,
+    },
+  },
+  {
+    files: ['**/*.mjs'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      globals: globals.node,
+      parserOptions: parserOptionsJs,
+      sourceType: 'module',
+    },
+    plugins: {
+      n: eslintPluginN,
+    },
+  },
+);
