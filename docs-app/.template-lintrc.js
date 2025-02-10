0a8e639c054f21ebe37b5f102b23603fa3fce250
ijlee2 <ijlee2@users.noreply.github.com>
2025-02-10 14:01:59 +0100

    chore: Updated project configurations

--- docs-app/.template-lintrc.js
+++ docs-app/.template-lintrc.js
@@ -1,6 +1,3 @@
 'use strict';
 
-module.exports = {
-  plugins: ['@ijlee2-frontend-configs/ember-template-lint'],
-  extends: ['@ijlee2-frontend-configs/ember-template-lint:recommended'],
-};
+module.exports = require('@ijlee2-frontend-configs/ember-template-lint');
