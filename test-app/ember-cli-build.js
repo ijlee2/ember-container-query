e0caad4e63d4d8c39065c981cdde300f34131bec
Isaac Lee <16869656+ijlee2@users.noreply.github.com>
2025-10-21 05:28:43 +0200

    Updated dependencies (#273)
    * chore: Updated dependencies
    
    * chore: Updated ember-source to 6.8.0
    
    * chore: Added lockfile
    
    * chore: Added changeset

--- test-app/ember-cli-build.js
+++ test-app/ember-cli-build.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const sideWatch = require('@embroider/broccoli-side-watch');
+const { maybeEmbroider } = require('@embroider/test-setup');
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function (defaults) {
@@ -20,8 +21,6 @@ module.exports = function (defaults) {
       }),
     },
   });
-
-  const { maybeEmbroider } = require('@embroider/test-setup');
 
   return maybeEmbroider(app);
 };
