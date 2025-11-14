15e9d1000750f96afb5f18f5b1e7f806241b6edd
Isaac Lee <16869656+ijlee2@users.noreply.github.com>
2025-11-14 15:47:52 +0100

    chore: Updated project configurations

--- packages/ember-container-query/unpublished-development-types/index.d.ts
+++ packages/ember-container-query/unpublished-development-types/index.d.ts
@@ -1,18 +1,2 @@
 // Add any types here that you need for local development only.
 // These will *not* be published as part of your addon, so be careful that your published code does not rely on them!
-
-import '@glint/environment-ember-loose';
-import '@glint/environment-ember-template-imports';
-
-import type EmberElementHelperRegistry from 'ember-element-helper/template-registry';
-
-import type EmberContainerQueryRegistry from '../src/template-registry.ts';
-
-declare module '@glint/environment-ember-loose/registry' {
-  export default interface Registry
-    extends EmberContainerQueryRegistry,
-      EmberElementHelperRegistry {
-    // Add any registry entries from other addons here that your addon itself uses (in non-strict mode templates)
-    // See https://typed-ember.gitbook.io/glint/using-glint/ember/using-addons
-  }
-}
